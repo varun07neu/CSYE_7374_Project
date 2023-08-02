a7ac06ea1d7838e1ac522760ff8b7955570f267f
Varun Venkatesh Gowda <gowda.v@northeastern.edu>
2023-08-02 17:48:03 -0400

    Initial commit- Created maven project with Driver

---
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -0,0 +1,10 @@
+package edu.neu.csye7374;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("============Main Execution Start===================\n\n");
+        
+
+        System.out.println("\n\n============Main Execution End===================");
+    }
+}
